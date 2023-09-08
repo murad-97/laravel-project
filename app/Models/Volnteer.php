e26a85cf87ae5aa45532be6c670c03160d15c8e6
murad-97 <muradalshorman136@gmail.com>
2023-09-08 20:45:35 +0300

    murad

--- app/Models/Volnteer.php
+++ app/Models/Volnteer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Volnteer extends Model
 {
     use HasFactory;
+    public function detail()
+    {
+        $this->hasMany(Volnteerdetail::class, 'volunteer_id', 'id');
+        
+    }
+    public function item()
+    {
+        $this->hasMany(Volnteeritem::class, 'volunteer_id', 'id');
+        
+    }
 }
