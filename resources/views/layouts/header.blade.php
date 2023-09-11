1bd5f0431562cc55e55e02ba5d5c6b8903ee8f4c
mohammad-alghzawi <mohd.a.ghzawi@gmail.com>
2023-09-11 09:02:11 +0300

    mohammad

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -21,8 +21,9 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href={{ asset('lib/flaticon/font/flaticon.css') }} rel="stylesheet">
 
-
-
+{{-- ================ --}}
+    {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
+{{-- ========================== --}}
 
 
     <link href={{ asset('lib/animate/animate.min.css') }} rel="stylesheet">
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,17 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
+    @extends('layouts.master')
+    @section('title' , 'Home')
+
+
+    @section('content')
     <x-app-layout>
-        <div class="hero overlay" style="background-image: url('{{ asset('img/1111.jpeg') }}'); height: 300px; background-size: cover; background-repeat: no-repeat;" >
+        <div class="hero overlay" style="  background-image: url('{{ asset('img/1111.jpeg') }}'); height: 300px; background-size: cover; background-repeat: no-repeat;" >
             <div class="container">
                 <div class="row align-items-center justify-content-center">
                     <div class="col-lg-6 text-center">
@@ -27,7 +21,7 @@
             </h2> --}}
         </x-slot>
     
-        <div class="py-12">
+        <div class="py-12" >
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
     
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
@@ -39,6 +33,7 @@
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         <table class="table table-hover">
+                            <h2 style="font-weight: bold;color:black">Your donations</h2>
                             <thead>
                                 <tr>
                                     <th scope="col">#</th>
@@ -84,11 +79,6 @@
             </div>
         </div>
     </x-app-layout>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
 
+    
+@endsection
