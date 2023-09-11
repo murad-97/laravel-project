1a98d4152e4c2c90e7e907754fc9e3d59da06e85
murad-97 <muradalshorman136@gmail.com>
2023-09-11 12:17:47 +0300

    murad

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,14 +63,14 @@
 </head>
 
 <body>
-    <section class="vh-100" style="background-color: #9A616D;">
+    <section class="vh-100" style="background-color: rgb(177,200,241);">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="https://images.pexels.com/photos/4021801/pexels-photo-4021801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                <img src="{{ asset("img/reg.jpeg") }}"
                                     alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height:600px" />
                             </div>
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
@@ -81,7 +81,7 @@
 
                                         <div class="d-flex align-items-center mb-3 pb-1">
                                             {{-- <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i> --}}
-                                            <span style="color: #9A616D"
+                                            <span style="color:#2958B0"
                                                 class="h1 fw-bold mb-0">HELPZ</span>
                                         </div>
 
