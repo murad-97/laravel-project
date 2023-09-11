0ffd963479004d2ef4df908315a17553aabf23a3
mohammad-alghzawi <mohd.a.ghzawi@gmail.com>
2023-09-11 16:49:52 +0300

    mohammad

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -25,13 +25,13 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
     
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                    <div class="">
                         @include('profile.partials.update-profile-information-form')
                     </div>
                 </div>
     
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                    <div class="">
                         <table class="table table-hover">
                             <h2 style="font-weight: bold;color:black">Your donations</h2>
                             <thead>
@@ -87,7 +87,7 @@
                 </div>
     
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                    <div class="">
                         @include('profile.partials.update-password-form')
                     </div>
                 </div>
