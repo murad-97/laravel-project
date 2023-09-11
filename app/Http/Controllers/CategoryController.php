373fd40f32132ac9ad8196ad081bfbcd084c1d44
sawsandagamseh <sawsan.dagamseh1@gmail.com>
2023-09-11 15:10:25 +0300

    sawsan

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Collection; 
 
 class CategoryController extends Controller
 {
