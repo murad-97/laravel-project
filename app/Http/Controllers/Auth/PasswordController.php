944880bce4b22f6bdb0863ea18a871b46012741c
murad-97 <muradalshorman136@gmail.com>
2023-09-13 09:53:25 +0300

    murad

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -17,7 +17,12 @@ class PasswordController extends Controller
     {
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'password' => [
+                'required',
+                'confirmed',
+                 Password::defaults(),
+                'regex:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+            ],
         ]);
 
         $request->user()->update([
